db2a3510ba5d799593b203d9d358437c653eb1ff
Zara <thezarazia@gmail.com>
2016-11-24 20:34:02 +0000

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'The StoryBoard Team'
+SITENAME = u'StoryBoard Blog'
+SITEURL = ''
+
+THEME = 'themes/storyboard-theme'
+
+PATH = 'content'
+
+TIMEZONE = 'GMT'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('StoryBoard', 'https://storyboard.openstack.org/#!/page/about'),
+         ('OpenStack', 'https://www.openstack.org/'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
